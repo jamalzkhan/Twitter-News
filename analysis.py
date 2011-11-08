8d8e0deafe382886b1750f3b3f4c50e356e8d9fa
Rafal Szymanski <me@rafal.io>
2011-11-08 02:31:08 +0000

    Word clouds for each period.
    Would be helpful to find a good blacklist for words
    
    Jamal, API my wordcloud data
    
    Also, a script to clean your database

--- analysis.py
+++ analysis.py
@@ -2,7 +2,9 @@ import threading
 import mongo_connector
 import shared
 import time
+import re
 import datetime
+import collections as col
 
 class Analysis(threading.Thread):
   def __init__(self,sleeptime=120,verbose=False):
@@ -13,6 +15,7 @@ class Analysis(threading.Thread):
     self.last_update = datetime.datetime.utcnow()
     self.sleeptime = sleeptime
     self.verbose = verbose
+    self.blacklist = ['http']
   
   def run(self):
     while 1:
@@ -26,6 +29,27 @@ class Analysis(threading.Thread):
       curr_time = datetime.datetime.utcnow()  
       self.add_new_time_period_to_stories(shared.stories, self.last_update, curr_time)
       self.last_update = curr_time
+      self.add_word_statistics_to_mongo(shared.stories, 0)
+
+  def add_word_statistics_to_mongo(self, stories, period):
+    """Adds words statistics to period 'period' for stories in 'stories'. 
+      All the stories in 'stories' should already be in the database"""
+
+    for story in stories:
+      period_data = self.stories_collection.find_one({"title" : story["title"]}, {"periods": {"$slice" : -1}})
+
+      tweet_text_list = []
+
+      for tweet_id in period_data['periods'][0]['tweets']:
+        tweet_text_list.append(self.tweet_collection.find_one({"_id" : tweet_id}, {"text" : 1})['text'])
+
+      text = " ".join([k for k in tweet_text_list]).lower()
+      words = [w for w in re.findall("\w{4,20}",text) if w not in self.blacklist]
+      count_data = dict(col.Counter(words).most_common(5))
+      print count_data
+
+      self.stories_collection.update({"title":story["title"],"periods.period" : period_data['periods'][0]['period']},{"$set": {"periods.$.wordcloud": count_data}},True)
+
 
   def add_stories_to_mongo(self, stories):
     """Goes through a list of stories and adds them to the stories in mongo. 
@@ -87,4 +111,4 @@ class Analysis(threading.Thread):
     for story in stories:
       if self.verbose:
         print "[INFO] Analysis Thread: Pushing period with {0} tweets to {1}".format(len(story["curr_period"]["tweets"]), story["title"])
-      self.stories_collection.update({"title":story["title"]},{"$push": {"periods": story["curr_period"]}})    
+      self.stories_collection.update({"title":story["title"]},{"$push": {"periods": story["curr_period"]}})
---
+++ dropDatabase.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mongo twitter_news --eval "db.dropDatabase()"
