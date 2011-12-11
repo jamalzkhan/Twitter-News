efb7b21a485d2d0a07ca65f4299d1611ab34d651
Jamal Khan <me@jamalkhan.com>
2011-12-11 04:00:12 +0000

    Basic testing working, to test, just run the rss_fetcher_test.py, off to bed

--- rss_fetcher_test.py
+++ rss_fetcher_test.py
@@ -12,30 +12,36 @@ class RssFetcherTest(unittest.TestCase):
     self.main_logger = logger.Logger()
     self.r_log = log.Log("RSS Test Fetcher", self.main_logger)
     
+    
     f = open(self.rss_file, 'r+')
     rss = f.read()
     # print rss
     f.close()
-    
+  
     self.rss = rss
-    
-    if (rss):
-      self.rss = rss_url
-    
+  
     self.rss_fetcher = rss_fetcher.RssFetcher(rss=self.rss, log=self.r_log)
       
   def test_rss_is_broken_url(self):
     """ Test to see what happens if the RSS Feed that is passed is broken"""
-    self.rss="http://thisisfake.com"
+    self.rss_fetcher.rss_link = "http://thisisfake.com"
     self.rss_fetcher.getNews()
+    print self.rss_fetcher.news_stories
     self.assertTrue(len(self.rss_fetcher.news_stories) == 0)
     
   def test_rss_returns_correct_format(self):
     """Test to see that given a dummy feed we get the correct stories"""
     self.rss_fetcher.getNews()
-    assert True
+    stories = self.rss_fetcher.news_stories
+    
+    
+    # Various checks for the dummy rss feed, whose values are known
+    
+    self.assertTrue(len(stories) == 2)
+    
   
     
 if __name__ == "__main__":
-    unittest.main()
+  print "Starting Unit Testing for RSS Fetcher Thread"
+  unittest.main()
   
