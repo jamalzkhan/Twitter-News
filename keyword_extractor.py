5de2faef6c59e1e1f0dad9f892edaac44505017c
Maciek Albin <madmck@gmail.com>
2012-01-12 18:56:45 +0000

    Special keyword blacklisting

--- keyword_extractor.py
+++ keyword_extractor.py
@@ -2,7 +2,7 @@ import ConfigParser, json, urllib, urllib2, itertools
 
 class KeywordExtractor:
   
-  def __init__(self, logger, limit=10):
+  def __init__(self, logger, limit=10, relevance=0.7):
     config = ConfigParser.RawConfigParser()
     config.read('config.conf')
     self.url = "http://access.alchemyapi.com/calls/url/URLGetRankedKeywords"
@@ -11,6 +11,7 @@ class KeywordExtractor:
     self.request_data["outputMode"] = 'json'
     self.request_data["maxRetrieve"] = limit
     self.request_data["sourceText"] = 'cleaned_or_raw'
+    self.relevance = relevance
     self.logger = logger
     self.keyword_blacklist = [line.strip() for line in open('keyword_blacklist.txt')]
     self.title_blacklist = [line.strip() for line in open('keyword_title_filter_list.txt')]
@@ -29,12 +30,13 @@ class KeywordExtractor:
         #We only care about the keywords
         keywords = []
         for keyword_entry in data['keywords']:
-          if (float(keyword_entry["relevance"]) >= 0.7):
+          if not self.keywordRelevant(keyword_entry):
+            continue
+          else:
             keyword = keyword_entry['text'].lower()
-            keyword_words = keyword.split()
-            keyword_words = itertools.ifilterfalse(lambda x: x in self.title_blacklist,keyword_words)
-            keyword = unicode(" ").join(keyword_words)
-            keywords.append(keyword)
+            keyword = self.removeTitles(keyword)
+            if (not keyword in self.keyword_blacklist):
+              keywords.append(keyword)
         return keywords
       else:
         self.logger.error(unicode("Error while parsing the response from Alchemy API. Response: {}").format(data))
@@ -46,6 +48,16 @@ class KeywordExtractor:
     except KeyError as strerror:
       self.logger.error(unicode("Problem with key: {}").format(strerror))
       return []
+      
+  def keywordRelevant(self, keyword_entry):
+    return float(keyword_entry["relevance"]) >= self.relevance
+  
+  def removeTitles(self, keyword):
+    keyword_words = keyword.split()
+    keyword_words = itertools.ifilterfalse(lambda x: x in self.title_blacklist,keyword_words)
+    return unicode(" ").join(keyword_words)
+    
+  
 
 if __name__ == "__main__":
   k = KeywordExtractor()
